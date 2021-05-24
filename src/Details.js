666ff99392a5ecd329c2c4a861073e49adc4065c
jacksnorlax <62570505+jacksnorlax@users.noreply.github.com>
2021-05-24 11:41:41 +0200

    Map function works
    Co-Authored-By: Alex Toth <alextothgithub@users.noreply.github.com>
    Co-Authored-By: null <Eriknicklasson@users.noreply.github.com>

--- src/Details.js
+++ src/Details.js
@@ -2,41 +2,64 @@ import React, { useState, UseRef, useEffect } from 'react';
 import { Modal } from 'react-bootstrap';
 
 
-export default function Details(props) {
-    const allInfo = props.pokeinfo[0];
-    const abilities = allInfo.abilities;
-    const stats = allInfo.stats;
-    const moves = allInfo.moves;
-    console.log(stats);
-    console.log(allInfo);
-    console.log(abilities);
+export default function Details(props, p) {
+  const allInfo = props.pokeinfo[0];
+  const abilities = allInfo.abilities;
+  const stats = allInfo.stats;
+  const moves = allInfo.moves;
+  const [showModal, setShowModal] = useState(true);
+  console.log(stats);
+  console.log(allInfo);
+  console.log(abilities);
 
 
 
-    abilities.forEach(function(abi) {
+  abilities.forEach(function (abi) {
     console.log(abi.ability.name)
 
   });
 
-    stats.forEach(function(sta) {
-      console.log(sta.stat.name + " " + sta.base_stat);
+  stats.forEach(function (sta) {
+    console.log(sta.stat.name + " " + sta.base_stat);
 
 
   });
 
-    moves.forEach(function(moves) {
+  moves.forEach(function (moves) {
 
 
   });
 
+  return (
 
+    < Modal show={true} >
+      <Modal.Header> Tjena</Modal.Header>
+      <Modal.Body>
 
+<<<<<<< HEAD
     return (
+=======
+        <div>
+          {props.pokeinfo[0].name}
+        </div>
+        <div>
+          Pokedex number {props.pokeinfo[0].id}
+        </div>
+        <div>
 
-        < Modal show={true} >
-            <Modal.Header> Tjena</Modal.Header>
-            <Modal.Body>
+        </div>
 
+
+
+      </Modal.Body>
+      <Modal.Footer>
+        <button type="button" className="btn btn-primary" onClick={props.hideModal}>  Close</button>
+      </Modal.Footer>
+>>>>>>> 5e6ba3e729a030da0fe33b88bbfb24e72941e694
+
+    </Modal >
+
+<<<<<<< HEAD
             <div>
               {allInfo.name}
             </div>
@@ -47,17 +70,11 @@ export default function Details(props) {
             <div>
               {abilities.map((data) => <p> {data.ability.name} </p>)}
             </div>
+=======
+
+>>>>>>> 5e6ba3e729a030da0fe33b88bbfb24e72941e694
 
 
 
-            </Modal.Body>
-            <Modal.Footer><button> Close</button></Modal.Footer>
-
-        </Modal >
-
-
-
-
-
-    )
+  )
 }
