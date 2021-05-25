7f4a450b0233616e7a968910cdbbf08a54c34167
jacksnorlax <62570505+jacksnorlax@users.noreply.github.com>
2021-05-25 10:57:38 +0200

    worked on local storage
    Co-Authored-By: null <Eriknicklasson@users.noreply.github.com>
    Co-Authored-By: Alex Toth <alextothgithub@users.noreply.github.com>

--- src/Team.js
+++ src/Team.js
@@ -4,20 +4,12 @@ import Search from './Search'
 
 export default function Team(props) {
 
-  var existingEntries = JSON.parse(localStorage.getItem("team"));
-  var entryTitle = props.pokeinfo[0].name
-  var entryText = props.pokeinfo[0].id;
-  console.log(entryTitle);
-  console.log(entryText);
-  var entry = {
-      "name": entryTitle,
-      "id": entryText
-  };
-  console.log(entry);
-  localStorage.setItem("entry", JSON.stringify(entry));
-  // Save allEntries back to local storage
-  existingEntries.push(entry);
-  localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+  var myTeam= [];
+  var first= {id:props.pokeinfo[0].id, name: props.pokeinfo[0].name};
+  myTeam.push(first);
+
+  localStorage.setItem('myTeam', JSON.stringify(myTeam));
+  var output = JSON.parse(localStorage.getItem('myTeam'));
 
     return (
 
